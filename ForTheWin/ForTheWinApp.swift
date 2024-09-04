78d1398a26d4104eaea18c0a0d1e03f35f42171a
Quinnium <github@quinnium.com>
2024-09-04 17:01:11 +0100

    Initial Commit

---
+++ ForTheWin/ForTheWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForTheWinApp.swift
+//  ForTheWin
+//
+//  Created by Quinn on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ForTheWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
